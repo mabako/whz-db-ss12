9a9f7410c858ae76f1b891ae130249209fa75ef6
Marcus Bauer <mabako@gmail.com>
2012-05-07 14:28:43 +0200

    Simple Hauptklasse, die momentan nichts tut.

---
+++ autohaus/src/net/mabako/zwickau/autohaus/Main.java
@@ -0,0 +1,14 @@
+package net.mabako.zwickau.autohaus;
+
+/**
+ * Hauptklasse für das Autohaus-Programm.
+ * 
+ * @author Marcus Bauer (mabako@gmail.com)
+ */
+public class Main {
+	/**
+	 * Startet das Programm.
+	 */
+	public static void main(String[] args) {
+	}
+}
