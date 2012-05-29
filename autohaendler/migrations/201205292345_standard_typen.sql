37c314b1186c966a07d0c921b8313b5181382800
Marcus Bauer <mabako@gmail.com>
2012-05-30 00:39:49 +0200

    Standard-Auto-Typen als Migration hinzugefügt.

---
+++ autohaendler/migrations/201205292345_standard_typen.sql
@@ -0,0 +1,14 @@
+SET IDENTITY_INSERT typ ON;
+INSERT INTO typ (id, name) VALUES
+	(1, 'Kleinstwagen'),
+	(2, 'Kleinwagen'),
+	(3, 'Kompaktklasse'),
+	(4, 'Mittelklasse'),
+	(5, 'Obere Mittelklasse'),
+	(6, 'Oberklasse'),
+	(7, 'Sportwagen'),
+	(8, 'Minivan'),
+	(9, 'Geländewagen'),
+	(10, 'Pickup'),
+	(11, 'Nutzfahrzeug');
+SET IDENTITY_INSERT typ OFF;
