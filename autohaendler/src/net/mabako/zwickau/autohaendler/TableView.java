d6a819aa979619437d17b140acd213f43424b037
Marcus Bauer <mabako@gmail.com>
2012-05-29 17:09:52 +0200

    Erweiterte Darstellung für Preis, Verbrauch und zurückgelegte Distanz

--- autohaendler/src/net/mabako/zwickau/autohaendler/TableView.java
+++ autohaendler/src/net/mabako/zwickau/autohaendler/TableView.java
@@ -1,8 +1,15 @@
 package net.mabako.zwickau.autohaendler;
 
+import java.awt.Component;
+import java.util.HashMap;
+
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.TableCellEditor;
+import javax.swing.table.TableCellRenderer;
+
 import net.mabako.zwickau.db.Prepared;
 import net.mabako.zwickau.db.Results;
 
@@ -31,15 +38,71 @@ public class TableView extends JPanel
 		JScrollPane scrollPane = new JScrollPane();
 		add(scrollPane, "cell 0 0,grow");
 		
-		table = new JTable();
 		
 		Prepared p = db.prepare("SELECT * FROM " + tableName);
 		Results model = p.executeWithResult();
 		model.setTableName(tableName);
-		table.setModel(model);
+		table = new JCustomTable(model);
 		table.setAutoResizeMode(JTable.AUTO_RESIZE_LAST_COLUMN);
 		p.close();
 		
 		scrollPane.setViewportView(table);
+	}
+	
+	private class JCustomTable extends JTable {
+		/**
+		 * Serial
+		 */
+		private static final long serialVersionUID = 1710037847160457126L;
+		
+		private HashMap<String, TableCellRenderer> renderers = new HashMap<String, TableCellRenderer>();
+		
+		public JCustomTable(Results model)
+		{
+			super(model);
+			
+			for(final String[] data : new String[][]{
+					new String[]{"%s€", "preis"},
+					new String[]{"%s km", "distanz"},
+					new String[]{"%s l/100km", "verbrauch"}
+				})
+			{
+				
+				TableCellRenderer renderer = new TableCellRenderer()
+				{
+					@Override
+					public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column)
+					{
+						DefaultTableCellRenderer.UIResource component = (DefaultTableCellRenderer.UIResource) getDefaultRenderer(Double.class).getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+						String text = component.getText();
+						if(text != null && text.length() > 0)
+							component.setText(String.format(data[0], text));
+						return component;
+					}
+				};
+				for(int i = 1; i < data.length; ++ i)
+				{
+					renderers.put(data[i], renderer);
+				}
+			}
+		}
+		
+		/**
+		 * Liefert einen Renderer für die Zeile und Spalte zurück.
+		 */
+		@Override
+		public TableCellRenderer getCellRenderer(int row, int column) {
+			TableCellRenderer renderer = renderers.get(getModel().getColumnName(column));
+			if(renderer != null)
+				return renderer;
+			
+			return super.getCellRenderer(row, column);
+		}
+		
+		@Override
+		public TableCellEditor getCellEditor(int row, int column) {
+			// TODO: Auswahlboxen für Hersteller
+			return super.getCellEditor(row, column);
+		}
 	}
 }
